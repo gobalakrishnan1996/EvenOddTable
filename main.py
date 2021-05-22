44fadc685d8b6cbaa13b723c7314ef457bf37059
gobalakrishnan1996 <gobalakrishnangv@gmail.com>
2021-05-22 19:17:54 +0530

    i

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+MAX_NUMBER = 100
+
+import math
+
+def main():
+    for i in range (100):
+        print (i+1)
+        if i % 2 == 0:
+            print ("is odd")
+        if i % 2 != 0:
+            print ("is even")
+
+
+
+
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
